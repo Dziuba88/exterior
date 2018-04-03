94379d07b932d91443ecdd305a1e4674db146e75
Dziuba88 <Dziuba88@Gmail.com>
2018-04-03 10:40:44 +0300

    sliders

--- bin/scripts/app.js
+++ bin/scripts/app.js
@@ -283,15 +283,17 @@ $(document).ready(function() {
     return false;
   });
 
-  $('.slider--partners .owl-carousel').owlCarousel({
+  /*$('.slider--partners .owl-carousel').owlCarousel({
     thumbs: true,thumbImage: true,items: 1,loop: false,nav: true,
     navText: ['<svg><use xlink:href="#arrow_left_circle"></use></svg>', '<svg><use xlink:href="#arrow_right_circle"></use></svg>']
-  });
+  });*/
 
-  $('.slider--clients .owl-carousel').owlCarousel({
+
+
+  /*$('.slider--clients .owl-carousel').owlCarousel({
     thumbs: true,thumbImage: true,items: 1,loop: true,nav: true,
     navText: ['<svg><use xlink:href="#arrow_left_circle"></use></svg>', '<svg><use xlink:href="#arrow_right_circle"></use></svg>']
-  });
+  });*/
 
   $('.slider--documents .owl-carousel').owlCarousel({
     thumbs: false, nav: true,loop:true,autoplay: true,autoplayTimeout: 2000,autoplayHoverPause: false,
@@ -472,3 +474,100 @@ $(document).ready(function() {
 });
 
 
+
+  var sync1 = $("#sync1");
+  var sync2 = $("#sync2");
+  var slidesPerPage = 5; //globaly define number of elements per page
+  var syncedSecondary = true;
+
+  sync1.owlCarousel({
+    items : 1,
+    slideSpeed : 2000,
+    nav: true,
+    thumbs: false,
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsiveRefreshRate : 200,
+    navText: ['<svg><use xlink:href="#arrow_left_circle"></use></svg>', '<svg><use xlink:href="#arrow_right_circle"></use></svg>'],
+  }).on('changed.owl.carousel', syncPosition);
+
+  sync2
+    .on('initialized.owl.carousel', function () {sync2.find(".owl-item").eq(0).addClass("current");})
+    .owlCarousel({
+    items : slidesPerPage,
+    dots: false,
+    nav: false,
+    thumbs: false,
+    smartSpeed: 200,
+    slideSpeed : 500,
+    slideBy: 1, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+    responsiveRefreshRate : 100
+  }).on('changed.owl.carousel', syncPosition2);
+  function syncPosition(el) {
+    var count = el.item.count-1;
+    var current = Math.round(el.item.index - (el.item.count/2) - .5);
+    if(current < 0) {current = count;}
+    if(current > count) {current = 0;}
+    sync2
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = sync2.find('.owl-item.active').length - 1;
+    var start = sync2.find('.owl-item.active').first().index();
+    var end = sync2.find('.owl-item.active').last().index();
+    if (current > end) {sync2.data('owl.carousel').to(current, 100, true);}
+    if (current < start) {sync2.data('owl.carousel').to(current - onscreen, 100, true);}
+  }
+  function syncPosition2(el) {if(syncedSecondary) {var number = el.item.index;sync1.data('owl.carousel').to(number, 100, true);}}
+  sync2.on("click", ".owl-item", function(e){e.preventDefault();var number = $(this).index();sync1.data('owl.carousel').to(number, 300, true);});
+
+
+  var cl1 = $("#cl1");
+  var cl2 = $("#cl2");
+  var slidesPerPage = 5; //globaly define number of elements per page
+  var syncedSecondary = true;
+
+  cl1.owlCarousel({
+    items : 1,
+    slideSpeed : 2000,
+    nav: true,
+    thumbs: false,
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsiveRefreshRate : 200,
+    navText: ['<svg><use xlink:href="#arrow_left_circle"></use></svg>', '<svg><use xlink:href="#arrow_right_circle"></use></svg>'],
+  }).on('changed.owl.carousel', syncPosition);
+
+  cl2
+    .on('initialized.owl.carousel', function () {cl2.find(".owl-item").eq(0).addClass("current");})
+    .owlCarousel({
+    items : slidesPerPage,
+    dots: false,
+    nav: false,
+    thumbs: false,
+    smartSpeed: 200,
+    slideSpeed : 500,
+    slideBy: 1, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+    responsiveRefreshRate : 100
+  }).on('changed.owl.carousel', syncPosition2);
+  function syncPosition(el) {
+    var count = el.item.count-1;
+    var current = Math.round(el.item.index - (el.item.count/2) - .5);
+    if(current < 0) {current = count;}
+    if(current > count) {current = 0;}
+    cl2
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = cl2.find('.owl-item.active').length - 1;
+    var start = cl2.find('.owl-item.active').first().index();
+    var end = cl2.find('.owl-item.active').last().index();
+    if (current > end) {cl2.data('owl.carousel').to(current, 100, true);}
+    if (current < start) {cl2.data('owl.carousel').to(current - onscreen, 100, true);}
+  }
+  function syncPosition2(el) {if(syncedSecondary) {var number = el.item.index;cl1.data('owl.carousel').to(number, 100, true);}}
+  cl2.on("click", ".owl-item", function(e){e.preventDefault();var number = $(this).index();cl1.data('owl.carousel').to(number, 300, true);});
